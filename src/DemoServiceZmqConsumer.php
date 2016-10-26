af63194aa810bb7f709ef31e001dea2ed468586f
Andy Kirkham <andy@spiders-lair.com>
2016-10-26 11:50:40 +0100

    first commit

---
+++ src/DemoServiceZmqConsumer.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace PhpScotland2016\Demo\Service\Impls\Zmq;
+
+use PhpScotland2016\Demo\Service\Interfaces\DemoServiceRequest;
+use PhpScotland2016\Demo\Service\Interfaces\DemoServiceResponse;
+use PhpScotland2016\Demo\Service\Interfaces\DemoServiceInterface;
+
+class DemoServiceZmq implements DemoServiceInterface
+{
+	protected $_context = null;
+	protected $_push = null;
+	protected $_pull = null;
+	protected $_run  = true;
+
+	public function __construct(/* ToDo, DI here. PRs gladly accepted :) */) {
+		$this->_context = new \ZMQContext(1, true);
+		$conn = "tcp://" .  $_ENV["ZMQ_BROKER"] .":". $_ENV["ZMQ_BROKER_BACK_PORT"];
+		$this->log("Connecting to $conn");
+		$this->_pull = $context->getSocket(\ZMQ::SOCKET_PULL, null);
+		$this->_pull->connect($conn);
+		$conn = "tcp://" .  $_ENV["CROSSBAR_HOST"] .":". $_ENV["CROSSBAR_ZMQ_PULL_PORT"];
+		$this->log("Connecting to $conn");
+		$this->_push = $context->getSocket(\ZMQ::SOCKET_PUSH, null);
+		$this->_push->connect($conn);
+		if(extension_loaded("pcntl")) {
+			pcntl_signal(SIGTERM, function($signo) {
+				$this->_run = false;
+			});
+		}
+	}
+
+	public function execute() {
+		while($this->_run) {
+			try {
+				$request = new DemoServiceRequest($this->recv());
+				$response = $this->handleRequest($request);
+				$this->send($response);
+			}
+			catch(\Exception $e) {
+				$this->log($e->getMessage());
+			}
+		}
+		$this->log("Terminating");
+	}
+
+	private function handleRequest(DemoServiceRequest $request) {
+		$service = new DemoServiceLocal;
+		return $service->handleRequest($request);
+	}
+
+	private function recv() {
+		$json = $this->_pull->recv(\ZMQ::MODE_NOBLOCK);
+		if(!is_string($json)) {
+			usleep(10); // No CPU 100% please.
+		}
+		else {
+			$this->log("RX:".$json);
+		}
+		return $json;
+	}
+
+	private function send(DemoServiceResponse $response) {
+		$this->log("TX:".$response->getJson());
+		$this->_push->send($response->getJson(), \ZMQ::MODE_NOBLOCK);
+	}
+
+	private function log($in) {
+		if(isset($_ENV["VERBOSE"]) && (int)$_ENV["VERBOSE"] == 1) {
+			error_log($in);
+		}
+	}
+}
+
---
+++ src/DemoServiceZmqProducer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PhpScotland2016\Demo\Service\Impls\Zmq;
+
+use PhpScotland2016\Demo\Service\Interfaces\DemoServiceRequest;
+use PhpScotland2016\Demo\Service\Interfaces\DemoServiceResponse;
+use PhpScotland2016\Demo\Service\Interfaces\DemoServiceInterface;
+
+class DemoServiceZmqProducer implements DemoServiceInterface
+{
+	public function handleRequest(DemoServiceRequest $request) {
+		$context = new \ZMQContext();
+		try {
+			$session_id = $request->getParam("session_id", null);
+			if(is_null($session_id)) {
+				throw new \Exception("No sessionid provided");
+			}
+			$conn = "tcp://".$_ENV["ZMQ_BROKER"].":" . $_ENV["ZMQ_BROKER_FRONT_PORT"];
+			$zmq = $context->getSocket(\ZMQ::SOCKET_REQ, null);
+			$zmq->connect($conn);
+			$zmq->send($request->get(), \ZMQ::MODE_NOBLOCK);
+			$res = $zmq->recv();
+			$times--;
+			$counter++;
+		}
+		catch(\Exception $e) {
+			error_log("Exception: ". $e->getMessage());
+		}
+		
+		$message = $request->getAsArray();
+		$message['result'] = 0;
+		$message['msg'] = 'See websocket response';
+		return new DemoServiceResponse($message);
+	}
+}
+
